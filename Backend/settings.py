5a0f5ad5a4d28d5a4bba1d68caae92a4b4ad50bb
pthapli999 <pthapi999@gmail.com>
2019-09-08 03:49:58 +0530

    testing 1 push

--- Backend/settings.py
+++ Backend/settings.py
@@ -39,11 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django.contrib.sites',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'rest_auth.registration',
+    'allauth',
+    'allauth.account',
     'Users',
     'Notice'
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
