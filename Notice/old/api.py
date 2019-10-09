77382adf032b8ca660687c8c1d1836b27a62b0cd
Divyanshu <divyanand96@gmail.com>
2019-10-09 06:45:33 +0530

    previoud app service mane setup

---
+++ Notice/old/api.py
@@ -0,0 +1,131 @@
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
+from rest_framework.response import Response
+
+from Notice import permissions as notice_permission
+from Notice.models import Notice, Image
+from Notice.paginations import NoticePagination
+from .serializers import NoticeSerializers, PublicNoticeSerializers, NoticeImageSerializers
+
+
+class NoticeViewSet(viewsets.ModelViewSet):
+    serializer_class = NoticeSerializers
+    queryset = Notice.objects.all()
+
+    parser_classes = [JSONParser, FormParser, MultiPartParser]
+
+    permission_classes = [
+        permissions.IsAuthenticated,
+        notice_permission.IsNoticeUser,
+    ]
+
+    def perform_create(self, serializer):
+        temp_dept = self.request.POST.getlist('department[]')
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data['user'] = self.request.user
+        if temp_dept.__len__() >= 1:
+            serializer.validated_data['department'] = temp_dept
+        # serializer.validated_data['department'] =
+        notice = serializer.save()
+        # print(notice)
+
+    def perform_update(self, serializer):
+        temp_dept = self.request.POST.getlist('department[]')
+        serializer.is_valid(raise_exception=True)
+        if temp_dept.__len__() >= 1:
+            serializer.validated_data['department'] = temp_dept
+        serializer.save()
+
+    def get_queryset(self):
+        queryset = self.queryset
+        query_set = queryset.filter(user=self.request.user)
+        return query_set
+
+
+class PublicNoticeAPI(generics.ListAPIView):
+    serializer_class = PublicNoticeSerializers
+    pagination_class = NoticePagination
+
+    model = serializer_class.Meta.model
+
+    queryset = Notice.objects.all()
+
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+    def get_queryset(self):
+        return self.queryset.filter(public_notice=True).order_by('-created_at')
+
+    # def get_queryset(self):
+    #     queryset = self.model.objects.filter(public_notice=True)
+    #     return queryset
+
+    # user = self.request.user
+    # queryset = self.model.objects
+    # if user.is_authenticated and user.user_type != "STUDENT":
+    #     queryset = queryset.filter(public_notice=False)
+    # return queryset.order_by('-title')
+
+
+class PrivateNoticeAPI(generics.ListAPIView):
+    serializer_class = PublicNoticeSerializers
+    pagination_class = NoticePagination
+
+    model = serializer_class.Meta.model
+
+    queryset = Notice.objects.all()
+
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.user_type == "STUDENT":
+            queryset = self.model.objects.filter(public_notice=True)
+            queryset = queryset.filter(department=user.student_user.department)
+        elif user.user_type == "DEPARTMENT":
+            queryset = self.model.objects.all()
+            queryset = queryset.filter(user=user)
+        elif user.user_type == "SOCIETY":
+            queryset = self.model.objects.all()
+            queryset = queryset.filter(user=user)
+        elif user.user_type == "FACULTY":
+            queryset = self.model.objects.filter(public_notice=False)
+            queryset = queryset.filter(department=user.faculty_user.department)
+        else:
+            queryset = self.model.objects.filter(public_notice=False)
+
+        # queryset = queryset.filter(department=user.faculty_user.department)
+        # if user.user_type in ["STUDENT", "SOCIETY"]:
+        #     queryset = queryset.filter(department=user.faculty_user.department)
+
+        # print(queryset.get().user)
+        return queryset.order_by('-created_at')
+
+        # user = self.request.user
+        # queryset = self.model.objects
+        # if user.is_authenticated and user.user_type != "STUDENT":
+        #     queryset = queryset.filter(public_notice=False)
+        # return queryset.order_by('-title')
+
+
+class DeleteNoticeImage(generics.DestroyAPIView):
+    serializer_class = NoticeImageSerializers
+
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    def get_queryset(self):
+        queryset = Image.objects.filter(id=self.kwargs['pk'], notice=self.kwargs['pk_notice'])
+        return queryset
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.deleted_at:
+            return Response("Cannot delete default system category", status=status.HTTP_400_BAD_REQUEST)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
---
+++ Notice/old/serializers.py
@@ -0,0 +1,145 @@
+from django.utils.timezone import now
+from rest_framework import serializers
+
+from Notice.models import Notice, Image
+
+
+class ImageSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = "__all__"
+
+
+class NoticeSerializers(serializers.ModelSerializer):
+    images = ImageSerializers(many=True, required=False)
+    images_list = serializers.SerializerMethodField('notice_images')
+
+    class Meta:
+        model = Notice
+        read_only_fields = ('user',)
+        fields = (
+            'id', 'title', 'description', 'date', 'time', 'venue', 'public_notice', 'department', 'images_list',
+            'images', 'is_event')
+
+    def notice_images(self, notice):
+        request = self.context.get('request')
+        data = notice.image_set.all()
+        if data:
+            return list(map(lambda d: {
+                'id': d.id,
+                'url': request.build_absolute_uri(d.image.url)
+            }, data))
+        return
+
+    def create(self, validated_data):
+        departments = ""
+        images_data = self.context.get('view').request.FILES
+        try:
+            img = validated_data.pop('images')
+        except KeyError:
+            pass
+        try:
+            departments = validated_data.pop('department')
+        except KeyError:
+            pass
+        notice = Notice.objects.create(**validated_data)
+        if departments:
+            notice.department.set(departments)
+        for image_data in images_data.values():
+            Image.objects.create(notice=notice, image=image_data)
+        return notice
+
+    def update(self, instance, validated_data):
+        images_data = self.context.get('view').request.FILES
+
+        if len(images_data) > 0:
+            temp_img = Image.objects.filter(notice=instance)
+            # for img in temp_img:
+            #     img.delete()
+            for image_data in images_data.values():
+                Image.objects.create(notice=instance, image=image_data)
+
+        try:
+            img = validated_data.pop('images')
+        except KeyError:
+            pass
+
+        try:
+            departments = validated_data.pop('department')
+            if departments:
+                instance.department.set(departments)
+        except KeyError:
+            pass
+
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+
+        instance.is_event = validated_data.get('is_event', instance.is_event)
+        instance.venue = validated_data.get('venue', instance.venue)
+        instance.date = validated_data.get('date', instance.date)
+        instance.time = validated_data.get('time', instance.time)
+
+        instance.public_notice = validated_data.get('public_notice', instance.public_notice)
+
+        instance.save()
+
+        return instance
+
+
+class PublicNoticeSerializers(serializers.ModelSerializer):
+    can_edit = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField('is_named_bar')
+    images = serializers.SerializerMethodField('notice_images')
+    datetime = serializers.SerializerMethodField('notice_created')
+    images_list = serializers.SerializerMethodField('notice_images_list')
+    profile = serializers.SerializerMethodField('user_profile')
+    created_at = serializers.DateTimeField(format="%d-%m-%Y, %H:%M")
+    time = serializers.TimeField(format="%H:%M")
+    date = serializers.TimeField(format="%d-%m-%Y")
+
+    class Meta:
+        model = Notice
+        fields = (
+            'id', 'title', 'description', 'is_event', 'date', 'time', 'venue', 'user', 'profile', 'images',
+            'images_list', 'department', 'public_notice', 'can_edit', 'created_at', 'datetime')
+
+    def get_can_edit(self, notice):
+        request = self.context.get('request')
+        if notice.user.user_type in ['DEPARTMENT', 'SOCIETY'] and notice.user == request.user:
+            return (now() - notice.created_at).days <= 1
+        return False
+
+    @staticmethod
+    def is_named_bar(notice):
+        return notice.user.name
+
+    @staticmethod
+    def notice_created(notice):
+        return notice.created_at
+
+    def user_profile(self, notice):
+        request = self.context.get('request')
+        if notice.user.profile:
+            return request.build_absolute_uri(notice.user.profile.url)
+
+    def notice_images(self, notice):
+        request = self.context.get('request')
+        data = notice.image_set.all()
+        if data:
+            return list(map(lambda d: request.build_absolute_uri(d.image.url), data))
+        return
+
+    def notice_images_list(self, notice):
+        request = self.context.get('request')
+        data = notice.image_set.all()
+        if data:
+            return list(map(lambda d: {
+                'id': d.id,
+                'url': request.build_absolute_uri(d.image.url)
+            }, data))
+        return
+
+
+class NoticeImageSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Notice
